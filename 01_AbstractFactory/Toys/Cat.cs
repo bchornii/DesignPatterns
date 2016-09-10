c39be490b13f2f449a8660bf0618b52d8cafb4e3
Bogdan Chorniy <bogdan.chorniy@gmail.com>
2016-09-10 21:04:11 +0300

    Code clean-up.

--- 01_AbstractFactory/Toys/Cat.cs
+++ 01_AbstractFactory/Toys/Cat.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _01_AbstractFactory
+﻿namespace _01_AbstractFactory
 {
     public abstract class Cat : AnimalToy
     {
