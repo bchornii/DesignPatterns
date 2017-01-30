ec4432f041ff162e174a1168de0480f1c33e8b8e
Bohdan <bogdan.chorniy@gmail.com>
2017-01-30 21:30:49 +0200

    Code clean up V1.4

--- 03_Singlton_Lazy_ThreadSafe/Program.cs
+++ 03_Singlton_Lazy_ThreadSafe/Program.cs
@@ -1,17 +1,17 @@
 ﻿namespace _03_Singlton_Lazy_ThreadSafe
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
             DoHardWork();
             System.Console.ReadLine();
         }
 
-        static void DoHardWork()
+        private static void DoHardWork()
         {
-            Logger logger = Logger.Instance;
-            HardProcessor process = new HardProcessor(1);
+            var logger = Logger.Instance;
+            var process = new HardProcessor(1);
             logger.Log("Hard process created.");
             process.ProcessTo(5);
             logger.Log("Hard process finished.");
